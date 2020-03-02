d56b1c620c775991affb12f336dc27e1d691b160
Ashanti Richardson <ashanti.richardson@gmail.com>
2020-03-02 17:44:31 +0100

    tried the routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,17 @@ Rails.application.routes.draw do
 
   root to: 'pages#home'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  Resources :parcels do
+    resources :orders, only: [:new, :create, :show]
+    resources :deliveries, only: [:new, :create]
+  end
+
+  Resources :orders, only: [:edit, :update, :destroy] do
+    resources :reviews, only: [:new, :create, :show]
+  end
+
+  resources :drivers, only: [:new, :create, :edit, :update, :destroy, :show, :index]
+  resources :deliveries, only: [:index, :show]
+  resources :reviews, only: [:index, :edit, :update, :destroy]
 end
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
