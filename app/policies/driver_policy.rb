4a83a4768e0da762f81d686de3c798902dc20630
Isabelle Rousseau <rousseau_isabel@hotmail.com>
2020-03-02 17:32:19 +0100

    added pundit to driver

---
+++ app/policies/driver_policy.rb
@@ -0,0 +1,22 @@
+#frozen_string_literal: true
+
+class DriverPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def create?
+    return true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+end
