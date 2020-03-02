055a65d7199d930938fbc31e2d2dd6b42dd0672a
Isabelle Rousseau <rousseau_isabel@hotmail.com>
2020-03-02 16:34:12 +0100

    added pundit to parcels controller

---
+++ app/policies/parcel_policy.rb
@@ -0,0 +1,22 @@
+# frozen_String_literal: true
+
+class ParcelPolicy < ApplicationPolicy
+
+  def create?
+    return true
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  get 'pages/home'
-  get 'parcels/index'
-  get 'parcels/new'
-  get 'parcels/create'
-  get 'parcels/show'
-  get 'parcels/edit'
-  get 'parcels/update'
-  get 'parcels/destroy'
+
+  root to: 'pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
