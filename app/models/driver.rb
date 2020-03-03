2db57aa107b1704394b338f5f84b21f3a774b863
Ashanti Richardson <ashanti.richardson@gmail.com>
2020-03-03 12:18:29 +0100

    orders

--- app/models/driver.rb
+++ app/models/driver.rb
@@ -1,4 +1,8 @@
 class Driver < ApplicationRecord
-
+  has_many :reviews
+  belongs_to :user
+  has_many :orders
+  has many :deliveries, through: :orders
+  has_many :parcels, through: :orders
   validates :name, :email, :phone_number, :current_longitude, :current_latitude, presence: true
 end
